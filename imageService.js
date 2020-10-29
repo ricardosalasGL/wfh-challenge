d4e785b3dd457ff69e1c5cde7911cdd12913d706
ricardosalasGL <ricardo.salas@gorillalogic.com>
2020-10-28 20:25:12 -0600

    Changing var to const

--- imageService.js
+++ imageService.js
@@ -1,7 +1,7 @@
-var imageService = (function () {
+const imageService = (function() {
   const figmaURL = "https://api.figma.com/v1/images/";
   const authToken = "137654-ed8ee072-bae8-4d5d-991d-6abe160b4296";
-  const imagesCode = 'zY7VsMb3kZwf0I1z2oqtoa';
+  const imagesCode = "zY7VsMb3kZwf0I1z2oqtoa";
 
   return {
     getSvgUrl: async () => {
